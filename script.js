c6d5b1ee2377840bcb166e9ae6935aa57c685e69
Henrique <kciqueks@gmail.com>
2024-02-02 00:03:04 -0300

    revisao feita

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const nome = prompt('Olá, qual é o seu nome?')
-let cpf = Number(prompt('Qual é o seu CPF?'))
-let idade = Number(prompt('Qual a sua idade?'))
-let telefone = Number(prompt('Digite seu Telefone Celular:'))
-const endereco = prompt('Qual é o seu endereço?')
-const email = prompt('Digite seu email:')
-const sexo = prompt('Qual seu sexo?')
-const cidade = prompt('Qual a sua cidade?')
-const estado = prompt('Qual o seu Estado?')
+const nome = prompt('Olá, qual é o seu nome?');
+let cpf = Number(prompt('Qual é o seu CPF?'));
+let idade = Number(prompt('Qual a sua idade?'));
+let telefone = Number(prompt('Digite seu Telefone Celular:'));
+const endereco = prompt('Qual é o seu endereço?');
+const email = prompt('Digite seu email:');
+const sexo = prompt('Qual seu sexo? (M/F)');
+const cidade = prompt('Qual a sua cidade?');
+const estado = prompt('Qual o seu Estado?');
 
-if (nome &&
-    cpf &&
-    idade && 
-    telefone && 
-    endereco && 
-    email && 
-    sexo && 
-    cidade && 
-    estado){
+if (!/\d/.test(nome) &&
+    (!isNaN(cpf) && cpf.toString().length >= 8 && cpf.toString().length <= 11) &&
+    (!isNaN(idade) && idade > 0) &&
+    (!isNaN(telefone) && telefone.toString().length <= 9 && telefone.toString().length <= 11) &&
+    endereco &&
+    email.includes('@') &&
+    !/\d/.test(cidade) &&
+    !/\d/.test(estado) &&
+    sexo.toUpperCase() == 'M' || sexo.toUpperCase() == 'F') {
+
     console.log(`Nome: ${nome}
 CPF: ${cpf}
-Idade: ${idade} anos
+Idade: ${idade} anoss
 Telefone: ${telefone}
 Email: ${email}
 Sexo: ${sexo}
@@ -28,8 +29,10 @@ Cidade: ${cidade}
 Estado: ${estado}
 
 Cadastro concluído com sucesso`);
-    alert('Cadastro concluído com sucesso')
+    alert('Cadastro concluído com sucesso');
+} else {
+    console.log('Dados inválidos')
+    alert('Dados inválidos');
 }
-else{
-    alert('Dados inválidos')
-}
+
+
